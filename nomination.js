3ae4dba6a511fd43ecd5b8263187f00e3c5ec78b
Shazil Arif <shazilarif94@gmail.com>
2021-01-11 10:38:08 -0500

    add more UI components and behavior

--- nomination.js
+++ nomination.js
@@ -1,5 +1,5 @@
 HTMLElement.prototype.inputIsEmpty = function(){
-    return this.textContent.trim().length === 0;
+    return this.value.trim().length === 0;
 }
 HTMLElement.prototype.hide = function(){
     this.style.display = 'none';
@@ -8,29 +8,41 @@ HTMLElement.prototype.show = function(){
     this.style.display = 'block'
 }
 
-function fetchMovieFromAPI(movieTitle){
-
+// &apikey=fdf22c9a
+// http://www.omdbapi.com/?t=home&apikey=fdf22c9a
+async function fetchMovieFromAPI(movieTitle){
+    const apikey = 'fdf22c9a';
+    const titleQueryParameter = `t=${movieTitle}`;
+    const apiKeyQueryParameter = `apiKey=${apikey}`
+    const URL = `http://www.omdbapi.com/?${titleQueryParameter}&${apiKeyQueryParameter}`;
 }
 
 window.onload = function(){
     const searchBtn = document.getElementById('searchBtn');
     const movieSeachNameInput = document.getElementById('movieSearchNameInput');
     const alertField = document.getElementById('enterTitleAlert')
-    const movieTitle = movieSeachNameInput.textContent;
-
+    const searchResultsContainer = document.getElementById('searchResultsContainer');
+    const spinner = document.getElementById('resultsWaitingSpinner');
+    const searchResultsList = document.getElementById('searchResultsList');
 
     // this is so that we can hide the alert message incase it was previously shown
     movieSeachNameInput.addEventListener('keyup', () => {
         alertField.hide();
     });
 
-    searchBtn.addEventListener('click', () => {
+    // even to fire on search button click
+    searchBtn.addEventListener('click', async () => {
+        // show alert message if movie title name was not given
         if(movieSeachNameInput.inputIsEmpty()){
             alertField.show();
         }
         else{
             // show spinner
-            fetchMovieFromAPI(movieTitle);
+            spinner.show();
+            const movieTitle = movieSeachNameInput.value;
+            await fetchMovieFromAPI(movieTitle);
+            spinner.hide();
+            searchResultsContainer.show();
         }
     });
 }
